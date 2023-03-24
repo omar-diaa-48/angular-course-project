faab7a289ee7ce72735301fce02612d357c579df
Omar <omar.elwakil@vodafone.com>
2023-03-24 14:45:21 +0200

    make use of recipe item and adjust recipes list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipesListComponent {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test', 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg'),
-    new Recipe('A test recipe', 'This is simply a test', 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg'),
+    new Recipe('A test recipe 1', 'This is simply the first test', 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg'),
+    new Recipe('A test recipe 2', 'This is simply the second test', 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg'),
   ];
 }
