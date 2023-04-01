21aab51050aa12662c4fc4fe768fe9271c89f5b5
Omar <omar.elwakil@vodafone.com>
2023-04-01 15:21:08 +0200

    adjust app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { LogginService } from './services/logging.service';
 
 @Component({
@@ -6,14 +7,36 @@ import { LogginService } from './services/logging.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'course-project';
   loadedFeature: string = 'recipe';
+  intervalSubscription: Subscription = new Subscription();
 
   constructor(private logginService: LogginService) { }
 
   onNavigate(featureSelected: string) {
     this.loadedFeature = featureSelected;
     this.logginService.log('Featured changed, currently ' + featureSelected);
+  }
+
+  ngOnInit(): void {
+    const intervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count > 3) {
+          observer.error(new Error('Count cant be greater that 3'))
+        }
+        count++;
+      }, 1000)
+    })
+
+    this.intervalSubscription = intervalObservable.subscribe((data) => {
+      console.log(data);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
   }
 }
