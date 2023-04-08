67e0e263363da4ca6592390e7458f7e396212a8a
Omar <omar.elwakil@vodafone.com>
2023-04-08 13:01:16 +0200

    install firebase

--- src/app/authenticate/authenticate.component.ts
+++ src/app/authenticate/authenticate.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 @Component({
   selector: 'app-authenticate',
   templateUrl: './authenticate.component.html'
 })
 export class AuthenticateComponent {
-  constructor() { }
+  authForm: FormGroup;
+
+  constructor() {
+    this.authForm = new FormGroup({
+      'email': new FormControl('', [Validators.required]),
+      'password': new FormControl('', [Validators.required]),
+    })
+  }
+
+  onSubmit() {
+    const auth = getAuth();
+
+    createUserWithEmailAndPassword(auth, this.authForm.value.email, this.authForm.value.password)
+      .then((userCredential) => {
+        console.log({ userCredential });
+      })
+      .catch((error) => {
+        console.log({ error });
+      });
+  }
 }
